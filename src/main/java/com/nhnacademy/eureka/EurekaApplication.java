d4391d5f5984ad077aa64953306a868655f4fefc
serrot <parksh568923@gmail.com>
2025-11-19 02:11:35 +0900

    메인 Eureka server 세팅 찐막

--- src/main/java/com/nhnacademy/eureka/EurekaApplication.java
+++ src/main/java/com/nhnacademy/eureka/EurekaApplication.java
@@ -14,9 +14,9 @@ package com.nhnacademy.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableDiscoveryClient
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
 
